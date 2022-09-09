769e6bd3d8236e54b1d3b2fcb900736a4f0a64d2
Trol326 <servey22@gmail.com>
2022-09-09 20:36:25 +0300

    main menu buttons and camera (not final)

--- Assets/Scripts/MainMenuSc.cs
+++ Assets/Scripts/MainMenuSc.cs
@@ -5,11 +5,16 @@ using Cinemachine;
 
 public class MainMenuSc : MonoBehaviour
 {
+    [Header("UI")]
+    [SerializeField]GameObject UIWeaponMenu;
+    [SerializeField]GameObject UICharacterMenu;
+    //_____________________________________________________________
+    [Header("Cameras")]
     [SerializeField] CinemachineBrain _cameraBrain;
     [SerializeField] CinemachineVirtualCamera _weaponCam;
-    
+    [SerializeField] CinemachineVirtualCamera _characterCam;
     CinemachineVirtualCamera _currentCam;
-
+    //_____________________________________________________________
     void Awake()
     {
         if(!_cameraBrain)return;
@@ -17,10 +22,29 @@ public class MainMenuSc : MonoBehaviour
     }
     public void ToWeaponButton()
     {
-
+        if(UICharacterMenu)UICharacterMenu.SetActive(false);
+        ChangneCameraTo(_weaponCam);
+        StartCoroutine(WaitForSec(1));
+        if(UIWeaponMenu)UIWeaponMenu.SetActive(true);
+    }
+    public void ToCharactersButton()
+    {
+        if(UIWeaponMenu)UIWeaponMenu.SetActive(false);
+        ChangneCameraTo(_characterCam);
+        StartCoroutine(WaitForSec(1));
+        if(UICharacterMenu)UICharacterMenu.SetActive(true);
     }
     void ChangneCameraTo(CinemachineVirtualCamera targetCam)
     {
-
+        if(_currentCam == targetCam)return;
+        if(_currentCam)_currentCam.m_Priority = 0;
+        targetCam.m_Priority = 10;
+        _currentCam = targetCam;
     }
+
+    IEnumerator WaitForSec(int sec)
+    {
+        yield return new WaitForSeconds(sec);
+    }
+
 }
